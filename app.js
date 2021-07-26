9f17bc15768da3fa7ceee2d49177b00729ab30b7
David Dai <za8234@gmail.com>
2021-07-26 23:44:42 +0800

    Adjust navbar(hover) for ios system.

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 let nav = document.querySelector("nav");
 let navLiAnchor = document.querySelectorAll("nav ul li a");
+let navLi = document.querySelectorAll("nav ul li");
 window.addEventListener("scroll", () => {
     if (window.pageYOffset != 0) {
         nav.style.backgroundColor = "rgba(0, 119, 182, 0.5)";
         navLiAnchor.forEach(a => {
             a.style.color = "white";
-            // a.style.transition = "0.2s ease";
         })
     } else {
         nav.style = "";
@@ -15,14 +15,24 @@ window.addEventListener("scroll", () => {
     }
 });
 
-navLiAnchor[0].addEventListener("click", () => {
+
+
+
+navLiAnchor[0].addEventListener("click", e => {
     let sectionFirstPage = document.querySelector("section.firstPage");
     sectionFirstPage.scrollIntoView({behavior: "smooth"});
-
 })
 
-navLiAnchor[1].addEventListener("click", () => {
+
+navLiAnchor[1].addEventListener("click", e => {
     let sectionSecondPage = document.querySelector("section.secondPage");
     sectionSecondPage.scrollIntoView({behavior: "smooth"});
 })
 
+
+var canHover = !(matchMedia('(hover: none)').matches);
+if (canHover) {
+    navLi.forEach(li => {
+        li.classList.add("Hover");
+    })
+}
